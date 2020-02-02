4b9707a815885d84fd37ceea1ec185d9b4752438
Vitaly Ibragimov <dlskrbn@gmail.com>
2020-02-02 23:23:22 +0500

    ansible-3

---
+++ ansible/enviroments/prod/inventory.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+INVENTORY_FILE_NAME="inventory.json"
+SCRIPT_DIR=$( dirname $0 )
+INVENTORY_FILE_PATH="${SCRIPT_DIR}/${INVENTORY_FILE_NAME}"
+
+getip() {
+  (cd ../terraform/prod; terraform state show "$1" | grep nat_ip | awk -F\" '{print $2}')
+}
+
+case $1 in
+'--list' )
+  APP_IP=$(getip module.app.google_compute_instance.app)
+  DB_IP=$(getip module.db.google_compute_instance.db)
+  cat ${INVENTORY_FILE_PATH} | sed "s/{{APP_IP}}/${APP_IP}/g" | sed "s/{{DB_IP}}/${DB_IP}/g"
+  ;;
+* )
+  ;;
+esac
+
